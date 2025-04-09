7399e0c6290d14e1a9fee882a5f93561d3274cc3
Felipe_Roufman <161071252+FelipeRoufman@users.noreply.github.com>
2025-04-09 13:06:08 -0300

    RayCast2D ainda nao funciona
    tentei fazer funcionar a parte do inimigo ver que tem uma parede e reverter o vetor de movimento dele, se descobrir como me avisar e corrigir o main/fork

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -10,19 +10,48 @@ const points_label_scene = preload("res://scenes/points_label.tscn")
 
 @onready var ray_cast_2d = $RayCast2D as RayCast2D
 @onready var animated_sprite_2d = $AnimatedSprite2D as AnimatedSprite2D
+@onready var left_wall_ray = $LeftWallRay as RayCast2D
+@onready var right_wall_ray = $RightWallRay as RayCast2D
+var is_alive = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if !is_alive: return
+	
 	position.x -= horizontal_speed * delta
+	
+	handle_movement_collisions()
+
+func handle_movement_collisions():
+	
+	if not ray_cast_2d.is_colliding():
+		reverse_direction()
+		return
+	
+	if moving_left() and left_wall_ray.is_colliding():
+		reverse_direction()
+	elif moving_right() and right_wall_ray.is_colliding():
+		reverse_direction()
+
+func moving_left() -> bool:
+	return horizontal_speed > 0
+
+func moving_right() -> bool:
+	return horizontal_speed < 0
+
+func reverse_direction():
+	horizontal_speed *= -1
+	animated_sprite_2d.flip_h = not animated_sprite_2d.flip_h
+	position.y += 1 
 
 func die():
+	is_alive = false
 	horizontal_speed = 0
 	vertical_speed = 0
 	animated_sprite_2d.play("dead")
 	
 func die_from_hit():
-
-	set_collision_layer_value(3, false)
+	is_alive = false
+	set_collision_layer_value(1, false)
 	set_collision_mask_value(1, false)
 
 	rotation_degrees = 180
